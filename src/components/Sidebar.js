f7e1f7fa57f2c9d151c48c5e616f606476486264
picpal <picpal@kakao.com>
2024-05-30 21:37:07 +0900

    styling

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,7 +3,6 @@ import styles from "../styles/Sidebar.module.css";
 const Sidebar = () => {
   return (
     <div>
-
       {/* Navigation Title */}
       <div>
         <h1>
@@ -14,32 +13,35 @@ const Sidebar = () => {
           />
         </h1>
       </div>
-      
+
       {/* Notes */}
       <div className={styles.mt_6}>
-        <h1 className={styles.note_title}>
+        <h1 className={styles.subject}>
           <input
             type="text"
             className={styles.border_0}
             defaultValue={"First Note"}
           />
         </h1>
-        <div>
-          <h2>Master Project Name</h2>
+        <div className={styles.note_wrapper}>
+          <h2 className={styles.note_title}>Master Project Name</h2>
           <div>
-            <h3>Sub Project Name</h3>
+            <h3 className={styles.note_title}>Sub Project Name</h3>
+            <ul>
+              <li>Developer Write file_#01</li>
+              <li>Developer Write file_#02</li>
+            </ul>
             <div>
-              <h4>Sub Project Name</h4>
+              <h4 className={styles.note_title}>Sub Project Name</h4>
               {/* pages */}
               <ul>
                 <li>Developer Write file_#01</li>
                 <li>Developer Write file_#02</li>
               </ul>
-            </div>  
+            </div>
           </div>
         </div>
       </div>
-
     </div>
   );
 };
